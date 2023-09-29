5b80bf7a49824730f24f7fde7b7c3cab43468df6
otonielemanuel <otonielemanuel21@gmail.com>
2023-09-29 23:26:53 +0100

    add data social midia

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -1,60 +1,19 @@
-import React from "react";
-import {
-  BiLogoGithub,
-  BiLogoLinkedinSquare,
-  BiLogoTwitter,
-  BiLogoInstagramAlt,
-  BiLogoFacebookCircle,
-} from "react-icons/bi";
+import socialData from "../data/social-div";
 
 export default function Social() {
   return (
     <ul className="flex flex-row gap-4">
-      <li>
-        <a
-          href=""
-          className="flex items-center  border-b transition-all text-gray-500 duration-500 hover:border-gray-500"
-        >
-          <BiLogoGithub size="18" />
-          <span className="ml-1">github</span>
-        </a>
-      </li>
-      <li>
-        <a
-          href=""
-          className="flex items-center  border-b transition-all text-gray-500 duration-500 hover:border-gray-500"
-        >
-          <BiLogoLinkedinSquare size="18" />
-          <span className="ml-1">linkedin</span>
-        </a>
-      </li>
-      <li>
-        <a
-          href=""
-          className="flex items-center  border-b transition-all text-gray-500 duration-500 hover:border-gray-500"
-        >
-          <BiLogoTwitter size="18" />
-          <span className="ml-1">twitter</span>
-        </a>
-      </li>
-      <li>
-        <a
-          href=""
-          className="flex items-center  border-b transition-all text-gray-500 duration-500 hover:border-gray-500"
-        >
-          <BiLogoInstagramAlt size="18" />
-          <span className="ml-1">instagram</span>
-        </a>
-      </li>
-      <li>
-        <a
-          href=""
-          className="flex items-center  border-b transition-all text-gray-500 duration-500 hover:border-gray-500"
-        >
-          <BiLogoFacebookCircle size="18" />
-          <span className="ml-1">facebook</span>
-        </a>
-      </li>
+      {socialData.map((data) => (
+        <li key={data.id}>
+          <a
+            href={data.link}
+            className="flex items-center  border-b transition-all text-gray-500 duration-500 hover:border-gray-500"
+          >
+            <p>{data.icon}</p>
+            <span className="ml-1">{data.name}</span>
+          </a>
+        </li>
+      ))}
     </ul>
   );
 }
---
+++ src/data/social-div.ts
@@ -0,0 +1,44 @@
+"use client"
+import {
+    BiLogoGithub,
+    BiLogoLinkedinSquare,
+    BiLogoTwitter,
+    BiLogoInstagramAlt,
+    BiLogoFacebookCircle,
+  } from "react-icons/bi";
+  
+
+const socialData = [
+    {
+        id: 1,
+        name: 'github',
+        link: 'https://github.com/euotiniel',
+        icon: 'icone'
+    },
+    {
+        id: 2,
+        name: 'linkedin',
+        link: 'https://linkedin.com/euotiniel',
+        icon: 'icone'
+    },
+    {
+        id: 3,
+        name: 'twitter',
+        link: 'https://x.com/euotiniel',
+        icon: 'icone'
+    },
+    {
+        id: 4,
+        name: 'instagram',
+        link: 'https://instagram.com/euotiniel',
+        icon: 'icone'
+    },
+    {
+        id: 5,
+        name: 'facebook',
+        link: 'https://facebook.com/euotiniel',
+        icon: 'icone'
+    }
+];
+
+export default socialData;
