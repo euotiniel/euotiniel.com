ec0809afc061742bf956167785e6bea2e3f8d1d6
otonielemanuel <otonielemanuel21@gmail.com>
2023-11-11 15:50:03 +0100

    feat(blog): add img

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -30,11 +30,15 @@ export default function Home() {
         {blogs.map((blog) => (
           <Link href={"/blog/" + blog.slug} passHref key={blog.slug}>
             <div className="mb-5">
-              <Subtitle>{truncateText(blog.meta.title, 50)}</Subtitle>
+              <h2 className="mt-2 sm:mt-4 md:mt-6 lg:mt-8 xl:mt-10 pb-2 font-extrabold text-base sm:text-base md:text-base lg:text-lg xl:text-lg tracking-tight first:mt-0">
+                {truncateText(blog.meta.title, 50)}
+              </h2>
               <p className="text-gray-400 text-sm">
                 {truncateText(blog.meta.description, 80)}
               </p>
-              <small className="my-auto text-gray-400 text-xs italic">{blog.meta.date}</small>
+              <small className="my-auto text-gray-400 text-xs italic">
+                {blog.meta.date}
+              </small>
             </div>
           </Link>
         ))}
