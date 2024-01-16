d69cb711c058dc88db720c23cd380ed6b487cdea
otonielemanuel <otonielemanuel21@gmail.com>
2024-01-16 22:31:07 +0100

    receber

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Layout from "@/src/Layout";
+import Layout from "@/src/layout";
 import { MDXProvider } from "@mdx-js/react";
 import NotFound from "@/src/content/404.mdx";
 
