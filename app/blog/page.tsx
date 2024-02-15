ad68bd6aacf34a9def2ecdb791c834f35ae70477
otonielemanuel <otonielemanuel21@gmail.com>
2024-02-15 20:48:30 +0100

    ...

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
           {blogs.map((blog) => (
             <Link href={"/blog/" + blog.slug} passHref key={blog.slug}>
               <div className="mb-5">
-                <h2 className="mt-2 sm:mt-4 md:mt-6 lg:mt-8 xl:mt-10 pb-2 font-semibold text-sm sm:text-sm md:text-sm lg:text-base xl:text-base tracking-wider first:mt-0">
+                <h2 className="mt-2 sm:mt-4 md:mt-6 lg:mt-8 xl:mt-10 pb-2 font-semibold text-sm sm:text-sm md:text-sm lg:text-base xl:text-base">
                   {truncateText(blog.meta.title, 55)}
                 </h2>
                 <p className="text-gray-400 text-sm opacity-40">
