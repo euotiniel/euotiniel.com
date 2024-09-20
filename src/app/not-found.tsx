0d7f5e97f82019513ae7c480b1f62cdf8d093885
euotiniel <otonielemanuel21@gmail.com>
2024-09-20 12:40:18 +0100

    .

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,10 +5,10 @@ export default function page() {
   return (
     <Layout>
         <p className='font-semibold text-[16.5px] leading-7 text-neutral-800 dark:text-neutral-300 [&:not(:first-child)]:mt-6'>404</p>
-        <p className='mb-8 text-[15.5px] leading-7 text-neutral-600 dark:text-neutral-400 [&:not(:first-child)]:mt-6'>
+        <p className='mb-8 text-[14.9px] leading-7 text-neutral-600 dark:text-neutral-400 [&:not(:first-child)]:mt-6'>
             A página solicitada não foi enocntrada! Clique no link para voltar.
         </p>
-        <Link href="/" className='transition-all border-b text-gray-500 duration-500 hover:border-gray-500'>
+        <Link href="/" className='transition-all border-b duration-500 text-[14.9px] leading-7 text-neutral-600 dark:text-neutral-400 [&:not(:first-child)]:mt-6'>
             euotiniel-@euotiniel-navegador-web:~$ cd ..</Link>
     </Layout>
   )
