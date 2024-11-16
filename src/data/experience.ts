054e2a84a478ab9c1866a35f7750a4483ab6248f
Otoniel Emanuel <otonielemanuel21@gmail.com>
2024-11-16 21:27:21 +0100

    Update experience.ts

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -3,7 +3,7 @@ const experiences = [
       company: 'INOKRI',
       logo: '/images/works/inokri.png',
       role: 'Front-end Developer',
-      period: '2023 - agora',
+      period: '2023 - 2024',
     },
     {
       company: 'Kudika',
