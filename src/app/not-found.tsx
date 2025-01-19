0619d7de7e3e8a22b127adc73664af386ce5af19
Otoniel Emanuel <otonielemanuel21@gmail.com>
2025-01-19 22:30:01 +0100

    Update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,12 +7,12 @@ export default function page() {
       <p className="text-[16.5px] font-semibold leading-7 text-neutral-800 dark:text-neutral-300 [&:not(:first-child)]:mt-6">
         404
       </p>
-      <p className="mb-8 text-[14.9px] leading-7 text-neutral-600 dark:text-neutral-400 [&:not(:first-child)]:mt-6">
+      <p className="mb-8 text-[14.5px] leading-7 text-neutral-600 dark:text-neutral-400 [&:not(:first-child)]:mt-6">
         A página solicitada não foi enocntrada! Clique no link para voltar.
       </p>
       <Link
         href="/"
-        className="border-b text-[14.9px] leading-7 text-neutral-600 transition-all duration-500 dark:text-neutral-400 [&:not(:first-child)]:mt-6"
+        className="border-b text-[14.5px] leading-7 text-neutral-600 transition-all duration-500 dark:text-neutral-400 [&:not(:first-child)]:mt-6"
       >
         euotiniel-@euotiniel-navegador-web:~$ cd ..
       </Link>
