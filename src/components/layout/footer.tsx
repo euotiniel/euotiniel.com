a254757141d587dcc4a19d8ab73d34192a1f6037
Otoniel Emanuel <otonielemanuel21@gmail.com>
2025-03-29 22:57:43 +0100

    Update footer.tsx

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,50 +1,63 @@
-"use client";
+'use client'
 
-import { useEffect, useState } from 'react';
-import { motion } from 'framer-motion';
-import Link from "@/components/links";
-import { calculateTimeElapsed } from '@/lib/timeUtils';
+import { useEffect, useState } from 'react'
+import { motion } from 'framer-motion'
+import Link from '@/components/links'
+import { calculateTimeElapsed } from '@/lib/timeUtils'
+import { calculateTimeElapsedStr } from '@/lib/strTimeUtils'
 
 export default function Footer() {
-  const [timeElapsed, setTimeElapsed] = useState('');
-  const [timeUnit, setTimeUnit] = useState('');
+  const [timeElapsed, setTimeElapsed] = useState('')
+  const [timeElapsedStr, setTimeElapsedStr] = useState('')
+  const [timeUnit, setTimeUnit] = useState('')
 
   useEffect(() => {
-    const storedStartTime = sessionStorage.getItem('startTime');
-    const currentTime = new Date().getTime();
+    const storedStartTime = sessionStorage.getItem('startTime')
+    const currentTime = new Date().getTime()
 
-    const startTime = storedStartTime ? parseInt(storedStartTime, 10) : currentTime;
+    const startTime = storedStartTime
+      ? parseInt(storedStartTime, 10)
+      : currentTime
     if (!storedStartTime) {
-      sessionStorage.setItem('startTime', currentTime.toString());
+      sessionStorage.setItem('startTime', currentTime.toString())
     }
 
     const updateTime = () => {
-      const elapsedTime = calculateTimeElapsed(startTime);
-      const newTimeUnit = getTimeUnit(elapsedTime);
-      setTimeElapsed(elapsedTime);
-      setTimeUnit(newTimeUnit);
-    };
+      const elapsedTime = calculateTimeElapsed(startTime)
+      const elapsedTimeSrt = calculateTimeElapsedStr(startTime)
+      const newTimeUnit = getTimeUnit(elapsedTime)
+      const newTimeUnitStr = getTimeUnitStr(elapsedTime)
+      setTimeElapsed(elapsedTime)
+      setTimeElapsedStr(elapsedTimeSrt)
+      setTimeUnit(newTimeUnit)
+      setTimeUnit(newTimeUnitStr)
+    }
 
-    const interval = setInterval(updateTime, 1000);
+    const interval = setInterval(updateTime, 1000)
 
-    return () => clearInterval(interval);
-  }, []);
+    return () => clearInterval(interval)
+  }, [])
 
   const getTimeUnit = (elapsedTime) => {
-    const [unit] = elapsedTime.split(' ');
-    return unit;
-  };
+    const [unit] = elapsedTime.split(' ')
+    return unit
+  }
+
+  const getTimeUnitStr = (elapsedTimeStr) => {
+    const [unit] = elapsedTimeStr.split(' ')
+    return unit
+  }
 
   return (
-    <footer className="flex flex-col md:flex-row items-center justify-between my-8 md:my-14 text-neutral-600 dark:text-neutral-400">
+    <footer className="my-8 flex flex-col items-center justify-between text-neutral-600 dark:text-neutral-400 md:my-14 md:flex-row">
       <div className="mt-9 sm:mb-3">
         <small className="text-xs leading-none">
-          2025 &copy;{" "}
+          2025 &copy;{' '}
           <Link olink="https://twitter.com/euotiniel">euotiniel</Link>
         </small>
       </div>
       <div className="mt-4 flex items-center text-center">
-      <div className="mr-2 h-2 w-2 rounded-full bg-green-300 dark:bg-green-800 animate-pulse"></div>
+        <div className="mr-2 h-2 w-2 animate-pulse rounded-full bg-green-500 dark:bg-green-700"></div>
         <div>
           <motion.p className="text-xs">
             conectado há{' '}
@@ -56,9 +69,17 @@ export default function Footer() {
             >
               {timeElapsed}
             </motion.span>
+            <motion.span
+              key={timeElapsedStr}
+              initial={{ opacity: 0, filter: 'blur(4px)' }}
+              animate={{ opacity: 1, filter: 'blur(0px)' }}
+              transition={{ duration: 0.4 }}
+            >
+              {timeElapsedStr}
+            </motion.span>
           </motion.p>
         </div>
       </div>
     </footer>
-  );
+  )
 }
