1f3eb82dd3fb7617b6647f9de4db891d0b7a8dbb
Otoniel Emanuel <otonielemanuel21@gmail.com>
2025-05-01 14:40:46 +0100

    Update work.tsx

--- src/components/work.tsx
+++ src/components/work.tsx
@@ -11,24 +11,38 @@ export default function Work() {
       {experiences.map((experience, index) => (
         <div key={index} className="flex w-full items-center justify-between">
           <div className="flex w-full items-center gap-2">
-            <div className="rounded-lg border border-neutral-100 p-2 dark:border-neutral-700/30">
+            {/* <div className="rounded-lg border border-neutral-100 p-2 dark:border-neutral-700/30">
               <Image
                 src={experience.logo}
                 alt={`${experience.company} logo`}
-                width={25}
-                height={25}
+                width={10}
+                height={10}
                 className="h-auto w-full filter grayscale dark:invert select-none"
               />
-            </div>
+            </div> */}
             <div className="flex w-full flex-col">
-              <span className="text-[13px] font-semibold leading-none text-neutral-600 dark:text-neutral-300">
+              {/* <span className="text-[13px] font-semibold leading-none text-neutral-600 dark:text-neutral-300">
                 {experience.company}
-              </span>
+              </span> */}
               <div className="flex items-center justify-between pt-1">
-                <span className="text-[13.3px] leading-none text-neutral-500 dark:text-neutral-400 opacity-50">
-                  {experience.role}
-                </span>
-                <span className="text-[12.5px] leading-none text-neutral-500 dark:text-neutral-400 opacity-50">
+                <div className="flex items-center gap-2">
+                  <span className="text-[13.8px] leading-none text-neutral-500 dark:text-neutral-400">
+                    {experience.role} at
+                    </span>
+                    <div className="rounded-lg border p-1 rotate-12 hover:rotate-0 transition duration-300 border-neutral-100 dark:border-neutral-700/30 cursor-pointer">
+                      <Image
+                        src={experience.logo}
+                        alt={`${experience.company} logo`}
+                        width={18}
+                        height={18}
+                        className="select-none grayscale filter dark:invert opacity-80"
+                      />
+                    </div>
+                  
+                </div>
+                <span className="mx-4 h-[1px] flex-grow border-t border-dashed border-neutral-800 opacity-50 dark:border-neutral-400"></span>
+
+                <span className="text-[12.5px] leading-none text-neutral-950 opacity-50 dark:text-neutral-400">
                   {experience.period}
                 </span>
               </div>
