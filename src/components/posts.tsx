e69f53f37b7062ff69ac724b42f25e5ba18c6d27
Otoniel Emanuel <otonielemanuel21@gmail.com>
2025-05-01 14:44:39 +0100

    Update posts.tsx

--- src/components/posts.tsx
+++ src/components/posts.tsx
@@ -11,16 +11,16 @@ function truncateText(text: string, maxLength: number) {
 
 export default function PostCard(post: Post) {
   return (
-    <article className='w-full'>
+    <article className="w-full">
       <Link href={post.url}>
         <div className="flex w-full flex-row items-center text-zinc-800 dark:text-zinc-200">
-          <span className="text-[15px] text-black dark:text-neutral-300 tracking-tight whitespace-nowrap">
-          {truncateText(post.title, 35)}
-            </span>
-          <span className="mx-4 h-[1px] flex-grow bg-neutral-700 opacity-10 dark:bg-neutral-400"></span>
+          <span className="whitespace-nowrap text-[15px] tracking-tight text-black dark:text-neutral-300">
+            {truncateText(post.title, 35)}
+          </span>
+          <span className="mx-4 h-[1px] flex-grow border-t border-dashed border-neutral-800 opacity-50 dark:border-neutral-400"></span>
           <time
             dateTime={post.date}
-            className="text-xs text-neutral-400 dark:text-neutral-500 select-none"
+            className="select-none text-xs text-neutral-400 dark:text-neutral-500"
             aria-label={`${format(parseISO(post.date), 'dd/MM/yyyy')}`}
           >
             {format(parseISO(post.date), 'dd/MM/yyyy')}
