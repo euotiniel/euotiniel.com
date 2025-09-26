85c3ac8e81bdade4c3f80e9d688365b3fa1381cc
Otoniel Emanuel <otonielemanuel21@gmail.com>
2025-09-26 14:20:35 +0100

    Update social.ts

--- src/data/social.ts
+++ src/data/social.ts
@@ -3,7 +3,7 @@ const socialLinks = [
     { href: "https://github.com/euotiniel", label: "GitHub", target: "_blank" },
     { href: "https://www.linkedin.com/in/euotiniel/", label: "LinkedIn", target: "_blank" },
     { href: "https://x.com/euotiniel", label: "X", target: "_blank" },
-    { href: "https://instagram.com/euotiniel", label: "Instagram", target: "_blank" },
+    // { href: "https://instagram.com/euotiniel", label: "Instagram", target: "_blank" },
   ];
 
   export default socialLinks;
