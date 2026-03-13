7f794686eafc8a28d2973380522fd976cc8a2b6d
Otoniel Emanuel <otonielemanuel21@gmail.com>
2026-03-13 11:16:36 +0100

    Update job roles to Portuguese translations

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -2,31 +2,31 @@ const experiences = [
     {
       company: 'Banco BIR',
       logo: '/images/works/bancobir_logo.jpg',
-      role: 'Programmer Analyst',
+      role: 'Analista Programador',
       period: '2025 - agora',
     },
     {
       company: 'Resoluto Digital',
       logo: '/images/works/resoluto.png',
-      role: 'Front-end Developer',
+      role: 'Desenvolvedor Front-end',
       period: '2025',
     },
     {
       company: 'INOKRI',
       logo: '/images/works/inokri.png',
-      role: 'Front-end Developer',
+      role: 'Desenvolvedor Front-end',
       period: '2023 - 2024',
     },
     {
       company: 'INEFOR',
       logo: '/images/works/inefor.png',
-      role: 'Web Development Instructor',
+      role: 'Instrutor de Desenvolvimento Web',
       period: '2023',
     },
     {
       company: 'Kudika Digital',
       logo: '/images/works/kudika.png',
-      role: 'Front-end & UI designer',
+      role: 'Front-end & UI Designer',
       period: '2022 - 2023',
     },
   ];
